01b0590e4484671ab695f7fe2b8cbe50aed07c5b
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-10-12 19:02:39 +0100

    reduce map height

--- components/pages/Home/Map/CovidMap.jsx
+++ components/pages/Home/Map/CovidMap.jsx
@@ -2,7 +2,7 @@ import ShowMap from "../../../Map/ShowMap";
 
 const CovidMap = () => {
     return (
-        <figure className="map p-1 rounded-md shadow-2xl bg-red-primary bg-opacity-20  h-[450px] lg:h-[700px] mt-10">
+        <figure className="map p-1 rounded-md shadow-2xl bg-red-primary bg-opacity-20  h-[450px] lg:h-[500px] mt-10 max-w-4xl mx-auto">
             <ShowMap />
         </figure>
     );
